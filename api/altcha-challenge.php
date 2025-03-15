2d4cb861b047d65a6d4c2e8059963f3b716f578d
Michal Ferber <michal.ferber@ari-integration.com>
2025-03-15 05:13:03 -0400

    Adding altcha to tools

--- api/altcha-challenge.php
+++ api/altcha-challenge.php
@@ -5,7 +5,7 @@ header('Content-Type: application/json');
 
 $challenge = [
   'algorithm' => 'SHA-256',
-  'challenge' => bin2hex(random_bytes(20)), // Increased to 20 bytes = 40 chars
+  'challenge' => bin2hex(random_bytes(20)), // 40 chars
   'salt' => bin2hex(random_bytes(8)),
   'complexity' => 10000,
   'signature' => ''
