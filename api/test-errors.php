1c7cb72ddaf90933d41991fb2b35a560efca959e
Michal Ferber <michal.ferber@ari-integration.com>
2025-03-17 01:27:12 -0400

    Updating error handling

---
+++ api/test-errors.php
@@ -0,0 +1,8 @@
+<?php
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', '/var/www/html/php_errors.log');
+error_reporting(E_ALL);
+trigger_error('Test error to verify logging', E_USER_ERROR);
+echo "This won't be shown if error logging works.";
+?>
