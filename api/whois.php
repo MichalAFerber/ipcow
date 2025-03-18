8b933700f9e33a1d5d4b7ec999fd5d25d65875ed
Michal Ferber <michal.ferber@ari-integration.com>
2025-03-18 05:49:07 -0400

    Updating tool

--- api/whois.php
+++ api/whois.php
@@ -31,109 +31,43 @@ debugLog("hCaptcha validation result: success");
 
 // Function to get WHOIS server based on TLD
 function getWhoisServer($domain) {
+    static $whoisServersCache = null;
+
+    // Extract the TLD from the domain
     $tld = strtolower(substr(strrchr($domain, '.'), 1));
-    $whoisServers = [
-        'com' => 'whois.verisign-grs.com',
-        'net' => 'whois.verisign-grs.com',
-        'edu' => 'whois.verisign-grs.com',
-        'org' => 'whois.pir.org',
-        'me' => 'whois.nic.me',
-        'co' => 'whois.nic.co',
-        'io' => 'whois.nic.io',
-        'us' => 'whois.nic.us',
-        'uk' => 'whois.nic.uk',
-        'ca' => 'whois.cira.ca',
-        'de' => 'whois.denic.de',
-        'fr' => 'whois.nic.fr',
-        'nl' => 'whois.domain-registry.nl',
-        'au' => 'whois.audns.net.au',
-        'nz' => 'whois.srs.net.nz',
-        'jp' => 'whois.jprs.jp',
-        'kr' => 'whois.kr',
-        'br' => 'whois.registro.br',
-        'se' => 'whois.iis.se',
-        'sg' => 'whois.sgnic.sg',
-        'cn' => 'whois.cnnic.cn',
-        'ru' => 'whois.tcinet.ru',
-        'ua' => 'whois.ua',
-        'ch' => 'whois.nic.ch',
-        'hk' => 'whois.hkirc.hk',
-        'tw' => 'whois.twnic.net.tw',
-        'id' => 'whois.pandi.or.id',
-        'my' => 'whois.mynic.my',
-        'th' => 'whois.thnic.co.th',
-        'vn' => 'whois.vnnic.vn',
-        'ph' => 'whois.dot.ph',
-        'ae' => 'whois.aeda.net.ae',
-        'sa' => 'whois.nic.net.sa',
-        'il' => 'whois.isoc.org.il',
-        'tr' => 'whois.nic.tr',
-        'it' => 'whois.nic.it',
-        'es' => 'whois.nic.es',
-        'pt' => 'whois.dns.pt',
-        'pl' => 'whois.dns.pl',
-        'cz' => 'whois.nic.cz',
-        'at' => 'whois.nic.at',
-        'be' => 'whois.dns.be',
-        'dk' => 'whois.dk-hostmaster.dk',
-        'fi' => 'whois.ficora.fi',
-        'no' => 'whois.norid.no',
-        'se' => 'whois.iis.se',
-        'nl' => 'whois.domain-registry.nl',
-        'ru' => 'whois.tcinet.ru',
-        'ro' => 'whois.rotld.ro',
-        'xyz' => 'whois.nic.xyz',
-        'site' => 'whois.nic.site',
-        'online' => 'whois.nic.online',
-        'store' => 'whois.nic.store',
-        'tech' => 'whois.nic.tech',
-        'website' => 'whois.nic.website',
-        'space' => 'whois.nic.space',
-        'press' => 'whois.nic.press',
-        'host' => 'whois.nic.host',
-        'fun' => 'whois.nic.fun',
-        'pw' => 'whois.nic.pw',
-        'top' => 'whois.nic.top',
-        'club' => 'whois.nic.club',
-        'guru' => 'whois.nic.guru',
-        'pro' => 'whois.afilias.net',
-        'info' => 'whois.afilias.net',
-        'mobi' => 'whois.afilias.net',
-        'name' => 'whois.nic.name',
-        'biz' => 'whois.neulevel.biz',
-        'us' => 'whois.nic.us',
-        'ws' => 'whois.website.ws',
-        'tv' => 'tvwhois.verisign-grs.com',
-        'cc' => 'ccwhois.verisign-grs.com',
-        'fm' => 'whois.nic.fm',
-        'nu' => 'whois.iis.nu',
-        'tk' => 'whois.dot.tk',
-        'ml' => 'whois.dot.ml',
-        'ga' => 'whois.dot.ga',
-        'cf' => 'whois.dot.cf',
-        'gq' => 'whois.dominio.gq',
-        'to' => 'whois.tonic.to',
-        'ai' => 'whois.ai',
-        'io' => 'whois.nic.io',
-        'sh' => 'whois.nic.sh',
-        'ac' => 'whois.nic.ac',
-        'sc' => 'whois2.afilias-grs.net',
-        'bz' => 'whois.belizenic.bz',
-        'hn' => 'whois.nic.hn',
-        'lc' => 'whois.afilias-grs.info',
-        'vc' => 'whois2.afilias-grs.net',
-        'mn' => 'whois.nic.mn',
-        'pw' => 'whois.nic.pw',
-        'tel' => 'whois.nic.tel',
-        'xxx' => 'whois.nic.xxx',
-        'aero' => 'whois.aero',
-        'coop' => 'whois.nic.coop',
-        'museum' => 'whois.museum',
-        'travel' => 'whois.nic.travel',
-        // Add more TLDs as needed
-        'default' => 'whois.iana.org'
-    ];
-    return $whoisServers[$tld] ?? $whoisServers['default'];
+    debugLog("Extracted TLD: $tld");
+
+    // Load WHOIS servers configuration if not already cached
+    if ($whoisServersCache === null) {
+        $whoisConfigFile = __DIR__ . '/whois-servers.json';
+        if (!file_exists($whoisConfigFile)) {
+            debugLog("Error: WHOIS servers configuration file not found: $whoisConfigFile");
+            $whoisServersCache = ['default' => 'whois.iana.org', 'servers' => []];
+        } else {
+            $whoisConfig = json_decode(file_get_contents($whoisConfigFile), true);
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                debugLog("Error: Failed to parse WHOIS servers configuration: " . json_last_error_msg());
+                $whoisServersCache = ['default' => 'whois.iana.org', 'servers' => []];
+            } else {
+                $whoisServersCache = $whoisConfig;
+            }
+        }
+        debugLog("Loaded WHOIS servers configuration into cache");
+    }
+
+    $defaultServer = $whoisServersCache['default'] ?? 'whois.iana.org';
+    $whoisServers = $whoisServersCache['servers'] ?? [];
+
+    // Search for the TLD in the servers array
+    foreach ($whoisServers as $server => $tlds) {
+        if (in_array($tld, $tlds, true)) {
+            debugLog("Found WHOIS server for TLD '$tld': $server");
+            return $server;
+        }
+    }
+
+    debugLog("No WHOIS server found for TLD '$tld', using default: $defaultServer");
+    return $defaultServer;
 }
 
 // Function to perform WHOIS lookup
